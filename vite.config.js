3a5898218685486b9628dda157d6adcde6bdef72
Your Name <you@example.com>
2023-06-23 18:57:50 +0200

    expose host and set port to 9000

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,17 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-const path = require("path");
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true,
+    port: 9000,
+  },
   resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
